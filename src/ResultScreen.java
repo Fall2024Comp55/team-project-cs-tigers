b31c5ec892f8f4bf1eb0fb83f463ae41f709621a
Ibrahim Tahir <ibrahimtahir497@gmail.com>
2024-10-30 16:36:34 -0700

    updates on the result screen class.

--- src/ResultScreen.java
+++ src/ResultScreen.java
@@ -1,4 +1,16 @@
 
 public class ResultScreen {
 
+public static void main (String[] args) {
+	System.out.println("---- Result Screen ----");
+	
+	if (user.score() > cpu.score()) {
+		System.out.println(user + " is the winner!!!");
+		System.out.println(user.score);
+	}
+	else {}
+	System.out.println("CPU wins! Try again!");
+	System.out.println(cpu.score);
+}
+
 }
