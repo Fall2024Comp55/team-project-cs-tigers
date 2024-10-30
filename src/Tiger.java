ca7c61b9fc3ddf13b297997e72d35791057018e0
Paul Marro <p_marro@u.pacific.edu>
2024-10-30 16:05:40 -0700

    added placeholders for all tiger class functions listed in the uml

--- src/Tiger.java
+++ src/Tiger.java
@@ -1,7 +1,9 @@
 import acm.graphics.*;
+
 public class Tiger {
 	public static final int hp = 100; //temp hp value change later
 	public static final int attack_val = 10; //temp attack value change later
+	public static final int speed = 1; //temp value that affects the character's movement speed if movement logic changes remove this
 	private GImage idle;
 	private GImage attack;
 	private GImage specialAttack;
@@ -14,8 +16,17 @@ public class Tiger {
 		return attack_val;
 	}
 	
+	public void attack() { //placeholder for attack function change/replace as needed
+		
+	}
 	
+	public void specialAttack() { //placeholder for special attack function change/replace as needed
+		
+	}
 	
+	public void spawnTiger() { //placeholder for spawnTiger function change/replace as needed
+	
+	}
 	
 	
 	
