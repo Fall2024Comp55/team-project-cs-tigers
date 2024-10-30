f6b556a8529504be404a678f043d2cea304eb2e3
Paul Marro <p_marro@u.pacific.edu>
2024-10-30 15:49:45 -0700

    Merge branch 'main' of https://github.com/Fall2024Comp55/team-project-cs-tigers.git into main

--- src/Tiger.java
+++ src/Tiger.java
@@ -1,9 +1,27 @@
-
+import acm.graphics.*;
 public class Tiger {
-
+	public static final int hp = 100; //temp hp value change later
+	public static final int attack_val = 10; //temp attack value change later
+	private GImage idle;
+	private GImage attack;
+	private GImage specialAttack;
+	
+	public int getHP() {
+		return hp;
+	}
+	
+	public int getAtkVal() {
+		return attack_val;
+	}
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
-
 }
+
