dc2b990d87c5d6b0b68ad1b0400a0b4112003832
FaizahM1 <f_muthana@u.pacific.edu>
2024-11-01 15:44:44 -0700

    added placeholder player class and updated result screen for score comparison and display

--- src/ResultScreen.java
+++ src/ResultScreen.java
@@ -1,5 +1,5 @@
 
-public class ResultScreen {
+/*public class ResultScreen {
 
 public static void main (String[] args) {
 	System.out.println("---- Result Screen ----");
@@ -13,4 +13,41 @@ public static void main (String[] args) {
 	System.out.println(cpu.score);
 }
 
+}
+*/
+public class ResultScreen {
+
+    public static void main(String[] args) {
+        System.out.println("---- Result Screen ----");
+
+        Player user = new Player("User", 50); // placeholder  
+        Player cpu = new Player("CPU", 25);   //placeholder
+
+        if (user.score() > cpu.score()) {
+            System.out.println(user.getName() + " is the winner!!!");
+            System.out.println("Score: " + user.score());
+        } else {
+            System.out.println("CPU wins! Try again!");
+            System.out.println("Score: " + cpu.score());
+        }
+    }
+}
+
+// demo placerholder 
+class Player {
+    private String name;
+    private int score;
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public int score() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
