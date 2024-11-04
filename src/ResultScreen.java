3c3a1c972abe07f8498d7ae93cd1008c06271ff9
Ibrahim Tahir <ibrahimtahir497@gmail.com>
2024-11-04 14:38:51 -0800

    Additions made to the result screen class.

--- src/ResultScreen.java
+++ src/ResultScreen.java
@@ -1,38 +1,15 @@
 
-/*public class ResultScreen {
-
-public static void main (String[] args) {
-	System.out.println("---- Result Screen ----");
-	
-	
-	//
-	if (user.score() > cpu.score()) {
-		System.out.println(user + " is the winner!!!");
-		System.out.println(user.score);
-	}
-	else {}
-	System.out.println("CPU wins! Try again!");
-	System.out.println(cpu.score);
-	
-	
-	//Credits - Made by Ibrahim, Faizah, Paul, Rogelio
-	System.out.println("Thank you for playing\n");
-	System.out.println("This game was made by Ibrahim, Faizah, Paul and Rogelio\n");
-}
-
-}
-*/
 public class ResultScreen {
 
     public static void main(String[] args) {
-        System.out.println("Result Screen");
+        System.out.println("----- Result Screen -----");
 
         Player user = new Player("User", 50); // placeholder  
         Player cpu = new Player("CPU", 25);   //placeholder
 
         if (user.score() > cpu.score()) {
             System.out.println(user.getName() + " is the winner!!!");
-            System.out.println("Score: " + user.score());
+            System.out.println("User Score: " + user.score());
         } else {
             System.out.println("CPU wins! Try again!");
             System.out.println("Score: " + cpu.score());
