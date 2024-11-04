72b80ac39b956997f6461439fabe132fb0bed2bc
Ibrahim Tahir <ibrahimtahir497@gmail.com>
2024-11-04 14:06:57 -0800

    added credits to the result screen.

--- src/ResultScreen.java
+++ src/ResultScreen.java
@@ -4,6 +4,8 @@ public class ResultScreen {
 public static void main (String[] args) {
 	System.out.println("---- Result Screen ----");
 	
+	
+	//
 	if (user.score() > cpu.score()) {
 		System.out.println(user + " is the winner!!!");
 		System.out.println(user.score);
@@ -11,6 +13,11 @@ public static void main (String[] args) {
 	else {}
 	System.out.println("CPU wins! Try again!");
 	System.out.println(cpu.score);
+	
+	
+	//Credits - Made by Ibrahim, Faizah, Paul, Rogelio
+	System.out.println("Thank you for playing\n");
+	System.out.println("This game was made by Ibrahim, Faizah, Paul and Rogelio\n");
 }
 
 }
