6fd7cbd80d5bc220773fca1ea52477dca05cd3ea
Paul Marro <p_marro@u.pacific.edu>
2024-11-06 14:15:54 -0800

    created the movement class and moved the movement from tiger to it

--- src/Tiger.java
+++ src/Tiger.java
@@ -3,16 +3,13 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 
-public class Tiger extends GraphicsProgram {
+public class Tiger {
 	public static final int hp = 100; //temp hp value change later
 	public static final int attack_val = 10; //temp attack value change later
 	public static final int speed = 1; //temp value that affects the character's movement speed if movement logic changes remove this
 	private GImage idle;
 	private GImage attack;
 	private GImage specialAttack;
-	public static final int PROGRAM_HEIGHT = 600;
-	public static final int PROGRAM_WIDTH = 800;
-	private GImage tigerTesting = new GImage("Robot.png", 300, 100); //image for movement testing
 	
 	public int getHP() {
 		return hp;
@@ -46,41 +43,8 @@ public class Tiger extends GraphicsProgram {
 	
 	//	}
 	
-	@Override
-	 public void keyPressed(KeyEvent e) {
-		 switch (e.getKeyCode()) {
-		 	case KeyEvent.VK_UP:
-		 		tigerTesting.move(0, -5); // Move up
-	            break;
-	        case KeyEvent.VK_DOWN:
-	            tigerTesting.move(0, 5); // Move down
-               break;
-           case KeyEvent.VK_LEFT:
-	            tigerTesting.move(-5, 0); // Move left
-	            break;
-	        case KeyEvent.VK_RIGHT:
-	            tigerTesting.move(5, 0); // Move right
-	            break;
-	        }
-	    }
-	
-	
-	
-	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
-	}	
-	
-	
-	public void run() {
-        add(tigerTesting);
-        addKeyListeners();
-		}
-	
-	
-	
 	public static void main(String[] args) {
-		new Tiger().start();
+	
 	}
 }
 
