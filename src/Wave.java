cd0225f40677c8aecf7c5e3f65879e488924f6d5
Rogelio <r_calderon3@u.pacific.edu>
2024-11-08 09:57:28 -0800

    Added more basic functions to Wave Class.

--- src/Wave.java
+++ src/Wave.java
@@ -1,13 +1,45 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import acm.graphics.GImage;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class Wave {
+	private GImage wave = new GImage("Robot.png",500,500);
 	private int hp = 100;
 	private String stageName = "Chris Kjeldsen Pool";
 	private int waveAttackValue = 10;
 	private int meleeValue = 5;
 	private int seaweedAttackValue = 15;
+	private double tigerLocX;
+	private double tigerLocY;
+	
+	public void setTigerLocation(double x, double y) {
+		tigerLocX = x;
+		tigerLocY = y;
+	}
 	
 	public String getStage() {
 		return stageName;
+	}
+	
+	public int getHP() {
+		return hp;
+	}
+	
+	public int getWaveAttackValue() {
+		return waveAttackValue;
+	}
+	
+	public int getMeleeValue() {
+		return meleeValue;
+	}
+	
+	public int getSeaweedValue() {
+		return seaweedAttackValue;
 	}
 
 	public static void main(String[] args) {
