8236cd13c86669269fb027ae955ba05520067471
Rogelio <r_calderon3@u.pacific.edu>
2024-11-08 09:42:59 -0800

    Started adding very Basic functions to Wave Class.

--- src/Wave.java
+++ src/Wave.java
@@ -1,5 +1,14 @@
 
 public class Wave {
+	private int hp = 100;
+	private String stageName = "Chris Kjeldsen Pool";
+	private int waveAttackValue = 10;
+	private int meleeValue = 5;
+	private int seaweedAttackValue = 15;
+	
+	public String getStage() {
+		return stageName;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
